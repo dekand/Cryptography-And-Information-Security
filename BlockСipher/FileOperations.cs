a44be889ad6f8d797f265da512ee5208e260c3ae
dekand <andekin@mail.ru>
2024-06-01 18:02:58 +0700

    update

---
+++ BlockСipher/FileOperations.cs
@@ -0,0 +1,42 @@
+﻿namespace BlockСipher
+{
+    internal class FileOperations
+    {
+        private const string DefPath = "C:\\Users\\Andrey\\Desktop\\1to10 level\\CryptographyAndInformationSecurity\\BlockСipher\\src\\";
+        public string Path { get; }
+        public string FileName { get; }
+
+        public FileOperations(string fileName)
+        {
+            Path = DefPath + fileName;
+            FileName = fileName;
+        }
+
+        public void Write(string? contents)
+        {
+            DisplayWrite();
+            File.WriteAllText(Path, contents);
+        }
+
+        public void Write(byte[] contents)
+        {
+            DisplayWrite();
+            File.WriteAllBytes(Path, contents);
+        }
+
+        public string Read()
+        {
+            DisplayRead();
+            return File.ReadAllText(Path);
+        }
+
+        public byte[] ReadBytes()
+        {
+            DisplayRead();
+            return File.ReadAllBytes(Path);
+        }
+
+        private void DisplayRead() => Console.WriteLine($"Выполняется чтение файла {FileName}...");
+        private void DisplayWrite() => Console.WriteLine($"Выполняется запись файла {FileName}...");
+    }
+}
---
+++ BlockСipher/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography;
+
+namespace BlockСipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 4; i++)
+            {
+                try
+                {
+                    var plaintextFile = new FileOperations($"plaintext{i}.txt");
+                    var decryptedFile = new FileOperations("decrypted.txt");
+                    var ciphertextFile = new FileOperations("ciphertext.txt");
+
+                    string original = plaintextFile.Read();
+
+                    if (!original.Any()) throw new Exception("Текст для шифрования не может быть пуст.");
+
+                    using (Aes myAes = Aes.Create())
+                    {
+                        byte[] encrypted = AES.Encrypt(original, myAes.Key, myAes.IV);
+                        ciphertextFile.Write(encrypted);
+
+                        string roundtrip = AES.Decrypt(encrypted, myAes.Key, myAes.IV);
+                        decryptedFile.Write(roundtrip);
+
+                        Console.WriteLine(new string('_', Console.WindowWidth));
+                        Console.WriteLine($"Исходный открытый текст ({plaintextFile.FileName}): {original}\n");
+                        Console.WriteLine($"Зашифрованный текст в байтах ({ciphertextFile.FileName}): {string.Join(" ", ciphertextFile.ReadBytes())}\n");
+                        Console.WriteLine($"Результат расшифрования ({decryptedFile.FileName}): {roundtrip}\n");
+                        Console.WriteLine(original == roundtrip ? "РАСШИФРОВАННЫЙ ТЕКСТ СОВПАЛ С ОРИГИНАЛОМ"
+                                                                : "РАСШИФРОВАННЫЙ ТЕКСТ НЕ СОВПАДАЕТ С ОРИГИНАЛОМ");
+                        Console.WriteLine(new string('_', Console.WindowWidth));
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine(ex.Message); }
+            }
+        }
+    }
+}
