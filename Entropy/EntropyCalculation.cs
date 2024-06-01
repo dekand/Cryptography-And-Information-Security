2b7a530147ada618270629644c7a826b28f6658d
dekand <andekin@mail.ru>
2024-06-01 13:24:07 +0700

    update

--- Entropy/EntropyCalculation.cs
+++ Entropy/EntropyCalculation.cs
@@ -1,20 +1,4 @@
-﻿//ПРАКТИЧЕСКОЕ ЗАДАНИЕ 2 ВЫЧИСЛЕНИЕ
-//ИНФОРМАЦИОННОЙ ЭНТРОПИИ ФАЙЛА
-//Цель задания – вычислить информационную энтропию различных файлов и
-//сопоставить результаты.
-
-//Задачи
-//1 Напишите программу, которая по заданному файлу определяет
-//частоты появления символов в нем.
-//2 Усовершенствуйте программу 1, чтобы она вычисляла энтропию этого
-//файла по вычисленным частотам.
-//3 Сгенерируйте несколько различных файлов. Например, из одинаковых
-//символов, из случайных символов 0 и 1, из случайных символов от 0 и
-//255 Попробуйте другие варианты. Убедитесь, что при большом
-//количестве одинаковых символов энтропия приближается к 0 В случае
-//случайных символов энтропия приближается к Log2(размера алфавита).
-
-namespace Entropy
+﻿namespace Entropy
 {
     internal class EntropyCalculation
     {
@@ -22,13 +6,11 @@ namespace Entropy
         public double E;
 
         private string fileText;
-        private double totalNum;
         private HashSet<char> uniqueValues;
 
         public EntropyCalculation(string filePath)
         {
             fileText = FileReader.Read(filePath);
-            totalNum = fileText.Length;
             uniqueValues = GetUniqueValues();
 
             Pi = GetProbabilities();
@@ -42,7 +24,7 @@ namespace Entropy
             {
                 foreach (var c in uniqueValues)
                 {
-                    probList.Add(fileText.Count(t => t == c) / totalNum);
+                    probList.Add(fileText.Count(t => t == c) / fileText.Length);
                 }
             }
             catch (Exception ex) { Console.WriteLine(ex.Message); }
@@ -68,10 +50,12 @@ namespace Entropy
 
         public void DisplayResult()
         {
-            Console.WriteLine($"Прочитанный текст из файла: {fileText}\n" +
-                              $"Уникальные символы: {string.Join(" ", uniqueValues)}\n" +
-                              $"Вероятности появления символов: \n\t{string.Join("\n\t", Pi)}\n" +
-                              $"Энтропия файла: {E}\n\n");
+            Console.WriteLine($"Прочитанный текст из файла: {fileText}\n\n" +
+                              $"Уникальные символы: {string.Join(" ", uniqueValues)}\n\n" +
+                              $"Вероятности появления символов: \n\t{string.Join("\n\t", Pi)}\n\n" +
+                              $"Энтропия файла: {E}\n\n"+
+                              $"Log2(размер алфавита): {Math.Log2(uniqueValues.Count)}\n");
+            Console.WriteLine(new string('_', Console.WindowWidth));
         }
 
         private HashSet<char> GetUniqueValues()
