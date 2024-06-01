54b0896c6486a99530fbe37afd3fd925e967a36a
dekand <andekin@mail.ru>
2024-06-01 15:14:28 +0700

    update

--- VernamСipher/RC4.cs
+++ VernamСipher/RC4.cs
@@ -63,9 +63,7 @@
     {
         public static void Swap<T>(this T[] array, int index1, int index2)
         {
-            T temp = array[index1];
-            array[index1] = array[index2];
-            array[index2] = temp;
+            (array[index1], array[index2]) = (array[index2], array[index1]);
         }
     }
 }
